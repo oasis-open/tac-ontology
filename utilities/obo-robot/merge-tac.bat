2504e9d2fb157409e735440ccc1cf1321194ec89
Ryan Hohimer <ryan.hohimer@semanticarts.com>
2024-01-31 11:37:38 -0800

    adding robot and sparql-anything utilities

---
+++ utilities/obo-robot/merge-tac.bat
@@ -0,0 +1,3 @@
+robot merge -vvv --input onts/tac.owl --input onts/stix.owl --input onts/tac-objects.owl --input onts/tac-properties.owl --input onts/adversary.owl --output output/tac.owl                                              
+
+
--- utilities/sparql-anything/sarun.bat
+++ utilities/sparql-anything/sarun.bat
@@ -1,3 +1,14 @@
 REM script to sparql-anything
 
-java -jar .\sparql-anything-0.8.1.jar -q .\mappings\%1.sparql -o .\output\%1.ttl
+rem make a copy of the input json, and rename it to my-input.json
+
+copy .\input\%1.json .\input\my-input.json 
+
+rem execute the sparql-anything script
+
+java -jar .\sparql-anything-0.8.1.jar -q .\mappings\json2ttl.sparql -o .\output\%1.ttl
+
+
+rem  delete the my-input.json file.
+
+del .\input\my-input.json
